85328ffc800ce89436f24727249b157064589af7
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-01 23:09:17 +0000

    Add view test to test index page status

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.shortcuts import get_object_or_404
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_index_is_rendered(self):
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "index.html")
