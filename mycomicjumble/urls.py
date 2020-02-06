a76b0848cb85120b27f30e93cb4449ec0bb0d317
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-06 01:15:02 +0000

    Add comics url to root urls

--- mycomicjumble/urls.py
+++ mycomicjumble/urls.py
@@ -13,13 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
+from django.views.static import serve
+from .settings import MEDIA_ROOT
 from home import urls as urls_home
 from home.views import index, about_view
+from comics import urls as urls_comics
+from comics.views import all_comics
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index),
     url(r'^about/', about_view),
+    url(r'^comics/', include(urls_comics)),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT})
 ]
