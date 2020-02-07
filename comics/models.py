fbfb79902c584f743aca4de72f24674b1ce0216f
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-07 18:21:15 +0000

    New model incl. images

--- comics/models.py
+++ comics/models.py
@@ -4,9 +4,12 @@ from django.db import models
 class Comic(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    grade = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images', blank=True, null=True)
+    condition = models.CharField(max_length=200)
+    grade = models.CharField(max_length=20)
+    penciler = models.CharField(max_length=50)
+    cover_artist = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
