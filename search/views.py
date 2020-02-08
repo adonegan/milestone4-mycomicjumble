d52b11d9c70cdb74d8b099451ce16bf54934f339
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-08 23:11:30 +0000

    Add logic for search

---
+++ search/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.db.models import Q
+from comics.models import Comic
+
+
+def do_search(request):
+    comics_list = Comic.objects.all()
+    query = request.GET.get('q')
+    if query:
+        comics_list = Comic.objects.filter(
+            Q(name__icontains=query) | Q(grade__icontains=query)
+        ).distinct()
+
+    context = {
+        'comics': comics_list
+    }
+    return render(request, "search.html", context)
