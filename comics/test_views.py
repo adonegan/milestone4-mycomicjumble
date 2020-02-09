c4e5c521f9013c6842cde4e1959a0405469e9e9e
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-09 22:11:08 +0000

    Add search view test

--- comics/test_views.py
+++ comics/test_views.py
@@ -9,3 +9,8 @@ class TestComicViews(TestCase):
         page = self.client.get("/comics/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "comics.html")
+
+    def test_get_search_page(self):
+        page = self.client.get("/search/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "search.html")
