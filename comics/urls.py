68995adfe6e12656306f2b55012de465f78087bd
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-10 15:50:42 +0000

    Modified pk in favour of comic_id

--- comics/urls.py
+++ comics/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import all_comics
+from .views import all_comics, detail
 
 urlpatterns = [
     url(r'^$', all_comics, name='comics'),
+    url(r'(?P<comic_id>\d+)/', detail, name='detail')
 ]
