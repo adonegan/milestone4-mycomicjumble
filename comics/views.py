a1d9e905c43625d12c14bccb427087d61987b2cb
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-10 15:50:57 +0000

    Add detail function for details page view

--- comics/views.py
+++ comics/views.py
@@ -5,3 +5,9 @@ from .models import Comic
 def all_comics(request):
     comics = Comic.objects.all()
     return render(request, "comics.html", {"comics": comics})
+
+
+def detail(request, comic_id):
+    """Function view for comic details page"""
+    comic = Comic.objects.get(pk=comic_id)
+    return render(request, "details.html", {"comic": comic})
