0dd04908161e72bf70bf80aeb09070d2b16e2725
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-13 15:24:49 +0000

    Remove comic reference

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,5 @@ from .views import view_cart, add_to_cart
 
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
-    url(r'^add/(?P<comic_id>\d+)', add_to_cart, name='add_to_cart'),
+    url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
 ]
