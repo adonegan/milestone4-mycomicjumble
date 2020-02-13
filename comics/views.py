19a192393d36baba683107f1efa1df442094d2db
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-13 23:41:37 +0000

    Paginate comics displaying on comics page

--- comics/views.py
+++ comics/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Comic
 
 
 def all_comics(request):
-    comics = Comic.objects.all()
+    """Show all comics but paginate after eight comics display on page"""
+    comics_list = Comic.objects.all()
+    paginator = Paginator(comics_list, 8)
+
+    page = request.GET.get('page')
+    try:
+        comics = paginator.page(page)
+    except PageNotAnInteger:
+
+        comics = paginator.page(1)
+    except EmptyPage:
+
+        comics = paginator.page(paginator.num_pages)
     return render(request, "comics.html", {"comics": comics})
 
 
