0dbc3537e456503e7472a4b793d6b75171cb353f
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-13 16:54:26 +0000

    Change redirect to comics page instead of index

--- cart/views.py
+++ cart/views.py
@@ -12,7 +12,7 @@ def add_to_cart(request, id):
     cart[id] = cart.get(id, quantity)
 
     request.session['cart'] = cart
-    return redirect(reverse('index'))
+    return redirect(reverse('comics'))
 
 
 def remove_from_cart(request, id):
