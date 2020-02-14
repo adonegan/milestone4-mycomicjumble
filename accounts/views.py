a3a71448528c24cc82cc9a29ed6fbb41a0c4d569
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-14 18:00:04 +0000

    Create login view

--- accounts/views.py
+++ accounts/views.py
@@ -6,3 +6,7 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "You are now logged out.")
     return redirect(reverse('index'))
+
+
+def login(request):
+    return render(request, 'login.html')
