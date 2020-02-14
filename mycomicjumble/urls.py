d58278e9f108f4a0ce724faeb4b0ec59ecb89d38
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-14 18:00:19 +0000

    Add login as view in top level urls file

--- mycomicjumble/urls.py
+++ mycomicjumble/urls.py
@@ -26,7 +26,7 @@ from search import urls as urls_search
 from search.views import do_search
 from cart import urls as urls_cart
 from cart.views import view_cart, add_to_cart
-from accounts.views import logout
+from accounts.views import logout, login
 
 
 urlpatterns = [
@@ -38,5 +38,6 @@ urlpatterns = [
     url(r'^cart/', include(urls_cart)),
     url(r'^comics/(?P<comic_id>[0-12]+)', detail, name='detail'),
     url(r'^accounts/logout/$', logout, name="logout"),
+    url(r'^accounts/login/$', login, name="login"),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT})
 ]
