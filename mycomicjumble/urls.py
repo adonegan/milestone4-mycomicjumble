33785ae5e05d99519a4992dc2e8f195482139589
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-14 17:47:38 +0000

    Add logout view and url to top level urls file

--- mycomicjumble/urls.py
+++ mycomicjumble/urls.py
@@ -26,6 +26,7 @@ from search import urls as urls_search
 from search.views import do_search
 from cart import urls as urls_cart
 from cart.views import view_cart, add_to_cart
+from accounts.views import logout
 
 
 urlpatterns = [
@@ -36,5 +37,6 @@ urlpatterns = [
     url(r'^search/', include(urls_search), name='search'),
     url(r'^cart/', include(urls_cart)),
     url(r'^comics/(?P<comic_id>[0-12]+)', detail, name='detail'),
+    url(r'^accounts/logout/$', logout, name="logout"),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT})
 ]
