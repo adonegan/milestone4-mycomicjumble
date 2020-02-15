9102aac2871aeb24665f5def6ce8a7ecb81a466d
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-15 20:48:17 +0000

    Test cart page view

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from comics.models import Comic
 
-# Create your tests here.
+
+class TestCartViews(TestCase):
+
+    def test_get_cart_page(self):
+        page = self.client.get("/cart/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "cart.html")
