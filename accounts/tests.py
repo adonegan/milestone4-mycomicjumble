055b5fffde3c06af2b472f38d00c936be739fdb1
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-15 20:49:47 +0000

    Add test for login page and form field requirement

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .forms import LoginForm
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_get_login_page(self):
+
+        page = self.client.get("/accounts/login/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "login.html")
+
+
+class TestForm(TestCase):
+
+    def test_field_must_be_filled_in(self):
+
+        form = LoginForm({'username': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['username'], [u'This field is required.'])
