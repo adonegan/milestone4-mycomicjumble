e35048d6f0e33837a4f4b536ad000032d8a5f164
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-19 01:14:58 +0000

    Ensure emails can be sent to backend

--- mycomicjumble/settings.py
+++ mycomicjumble/settings.py
@@ -136,3 +136,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
