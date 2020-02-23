855ccf7b97a20da02e80af3bcc30308c5484d00d
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-23 23:24:27 +0000

    Import view and add contact page app url

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import index, about_view
+from .views import index, about_view, contact_view
 
 urlpatterns = [
     url(r'^index/$', index, name='index'),
     url(r'^about/$', about_view, name='about'),
+    url(r'^contact/$', contact_view, name='contact'),
 ]
