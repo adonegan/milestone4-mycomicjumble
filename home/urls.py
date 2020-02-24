c17d667742177bb3d4aea6c1f0524259789d4443
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-24 17:22:02 +0000

    Add app level url for faqs view

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from .views import index, about_view, contact_view, glossary_view, privacy_view, terms_view
+from .views import index, about_view, contact_view, glossary_view, privacy_view, terms_view, faqs_view
 
 urlpatterns = [
     url(r'^index/$', index, name='index'),
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^glossary/$', glossary_view, name='glossary'),
     url(r'^privacy/$', privacy_view, name='privacy'),
     url(r'^terms/$', terms_view, name='terms'),
+    url(r'^faqs/$', faqs_view, name='faqs'),
 ]
