fe4666ef9255df90be1b94946de2f09fbb7b64b2
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-24 16:36:32 +0000

    Add app level urls for terms / privacy policies

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
-from .views import index, about_view, contact_view, glossary_view
+from .views import index, about_view, contact_view, glossary_view, privacy_view, terms_view
 
 urlpatterns = [
     url(r'^index/$', index, name='index'),
     url(r'^about/$', about_view, name='about'),
     url(r'^contact/$', contact_view, name='contact'),
     url(r'^glossary/$', glossary_view, name='glossary'),
+    url(r'^privacy/$', privacy_view, name='privacy'),
+    url(r'^terms/$', terms_view, name='terms'),
 ]
