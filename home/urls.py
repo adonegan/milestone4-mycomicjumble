97887aeb2d0deb8d7cc3b190f046941b47119866
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-24 01:52:30 +0000

    Add app level URL for glossary page

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import index, about_view, contact_view
+from .views import index, about_view, contact_view, glossary_view
 
 urlpatterns = [
     url(r'^index/$', index, name='index'),
     url(r'^about/$', about_view, name='about'),
     url(r'^contact/$', contact_view, name='contact'),
+    url(r'^glossary/$', glossary_view, name='glossary'),
 ]
