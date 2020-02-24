02e273b0cbb4838e93f0a336d844f290e74022bb
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-24 16:35:58 +0000

    Create views for terms and privacy policy pages

--- home/views.py
+++ home/views.py
@@ -18,6 +18,17 @@ def contact_view(request):
     contact_form = ContactForm
     return render(request, "contact.html", {'form': contact_form})
 
+
 def glossary_view(request):
     """A view that displays the glossary page"""
     return render(request, "glossary.html")
+
+
+def privacy_view(request):
+    """A view that displays the privacy page"""
+    return render(request, "privacy.html")
+
+
+def terms_view(request):
+    """A view that displays the terms and conditions page"""
+    return render(request, "terms.html")
