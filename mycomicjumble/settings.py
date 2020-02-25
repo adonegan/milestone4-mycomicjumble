6312f098249b410cdb23c73429b0b87a47ea06ae
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-25 15:52:11 +0000

    fixing error comment out

--- mycomicjumble/settings.py
+++ mycomicjumble/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'mycomicjumble.wsgi.application'
 #       'ENGINE': 'django.db.backends.sqlite3',
 #       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #   }
-#}
+# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
