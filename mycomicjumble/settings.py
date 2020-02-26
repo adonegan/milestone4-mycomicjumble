f0a579fbe14addd3a06efa104dc5346297e3f8f5
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-26 00:02:58 +0000

    Remove development if / else

--- mycomicjumble/settings.py
+++ mycomicjumble/settings.py
@@ -12,14 +12,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-from os import path
-if path.exists("env.py"):
-    import env
-
-if os.environ.get('DEVELOPMENT'):
-    development = True
-else:
-    development = False
+# from os import path
+# if path.exists("env.py"):
+#       import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -32,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('HOSTNAME'), '127.0.0.1']
 
@@ -93,15 +88,19 @@ WSGI_APPLICATION = 'mycomicjumble.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
-if development:
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    print("Postgres URL not found, using sqlite instead")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-else:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
