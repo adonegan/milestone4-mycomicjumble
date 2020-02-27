d87c55edb51f0f90f237f4a49131ffbb4f43a311
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-27 02:27:26 +0000

    Remove model test test

--- comics/test_models.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from .models import Comic
-
-
-class ComicSubmitTest(TestCase):
-    """ To test Comic model returns comic name """
-    def test_test_title(self):
-        comic = Comic(name="Comic")
-        self.assertEqual(str(comic), comic.name)
