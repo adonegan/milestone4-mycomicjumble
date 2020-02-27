88b123d946d7d5ee6550b4be64a135ec1dfeed8f
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-27 02:25:47 +0000

    Test Comics model

---
+++ comics/tests/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from comics.models import Comic
+
+
+class ComicModelTest(TestCase):
+
+    def test_string_representation(self):
+        comic = Comic(name="Spider-Man (2019) #1")
+        self.assertEqual(str(comic), comic.name)
+
+    def test_details_url(self):
+        pk = 2
+        name = "Spider-Man (2019) #1"
+        expected_result = '/comics/details/' + str(pk)
+        comic = Comic(pk=pk, name=name)
+        result = '/comics/details/2'
+        self.assertEqual(result, expected_result)
