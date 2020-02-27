0467400b2f7c4baca66e565def58a410e062293d
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-27 14:58:44 +0000

    Remove SSL for email

--- mycomicjumble/settings.py
+++ mycomicjumble/settings.py
@@ -174,7 +174,6 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
