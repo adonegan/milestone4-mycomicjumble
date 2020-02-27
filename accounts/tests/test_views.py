2d6115d785efdb43073487a9d960ce4c5f1f4c0d
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-27 02:28:48 +0000

    Test accounts views

---
+++ accounts/tests/test_views.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.shortcuts import get_object_or_404
+
+
+class TestAccountsViews(TestCase):
+
+    def test_get_login_page(self):
+        page = self.client.get("/accounts/login/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "login.html")
+
+    def test_get_registration_page(self):
+        page = self.client.get("/accounts/register/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "registration.html")
