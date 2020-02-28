e31aa7dfae7fdae99911a56aca1751538d15a169
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-28 22:47:55 +0000

    Remove app level contact url

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url, include
-from .views import index, about_view, contact_view, glossary_view, privacy_view, terms_view, faqs_view
+from .views import index, about_view, glossary_view, privacy_view, terms_view, faqs_view
 
 urlpatterns = [
     url(r'^index/$', index, name='index'),  # so visitors can access each page
     url(r'^about/$', about_view, name='about'),
-    url(r'^contact/$', contact_view, name='contact'),
     url(r'^glossary/$', glossary_view, name='glossary'),
     url(r'^privacy/$', privacy_view, name='privacy'),
     url(r'^terms/$', terms_view, name='terms'),
