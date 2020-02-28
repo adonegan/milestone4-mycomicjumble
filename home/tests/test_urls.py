a69c98c1faab6d8a10c92576042d27e10524333c
”adonegan” <48014273+adonegan@users.noreply.github.com>
2020-02-28 22:48:45 +0000

    Remove top level contact url

--- home/tests/test_urls.py
+++ home/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from home.views import index, about_view, contact_view, glossary_view, privacy_view, terms_view, faqs_view
+from home.views import index, about_view, glossary_view, privacy_view, terms_view, faqs_view
 
 
 class TestHomeUrls(SimpleTestCase):
@@ -13,10 +13,6 @@ class TestHomeUrls(SimpleTestCase):
     def test_about_view_url_is_resolved(self):
         url = reverse('about')
         self.assertEqual(resolve(url).func, about_view)
-
-    def test_contact_view_url_is_resolved(self):
-        url = reverse('contact')
-        self.assertEqual(resolve(url).func, contact_view)
 
     def test_glossary_view_url_is_resolved(self):
         url = reverse('glossary')
